ae695919884f3be08241ade55f31149b3d10cb56
xrys2021 <chwelter@eng.ucsd.edu>
2021-07-12 08:57:51 -0700

    adding in relationshipInspector + NodeMap Assets

---
+++ Assets/NodeMap/Scripts/Data/NodeTypeData.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace JSNodeMap {
+	[CreateAssetMenu(menuName = "Node Map/Node Type Data")]
+	public class NodeTypeData : ScriptableObject {
+		public NodeType[] nodeTypes;
+		public NodeType[] markerTypes;
+
+		public Material meshMaterial;
+		public Material lineMaterial;
+
+		public string[] NodeTypeNames {
+			get {
+				return nodeTypes.Select(t => t.name).ToArray();
+			}
+		}
+
+		public string[] MarkerTypeNames {
+			get {
+				return markerTypes.Select(t => t.name).ToArray();
+			}
+		}
+	}
+
+	[System.Serializable]
+	public class NodeType {
+		public string name;
+		public Sprite sprite;
+		public Mesh mesh;
+	}
+}
